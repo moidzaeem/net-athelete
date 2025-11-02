07d56f3a5ad45c22ef68a75914bd576098b9374e
Faruk <faruk@Faruk-Air.bbrouter>
2025-11-02 03:14:47 +0300

    Destructive push of all files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: { spacing: { 7.5: "1.875rem", 15: "3.75rem", 50: "12.5rem" } },
-  },
-  plugins: [],
-};
